ad69f729ff764a7772fea6307a322c8f422c81fb
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-11 22:50:24 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactHookFormExample } from "./components/react-hook-form-example";
+import { ReactHookFormExample2 } from "./components/react-hook-form-example2";
+
+export default function App() {
+  return (
+    <>
+      <ReactHookFormExample />;
+      <ReactHookFormExample2 />;
+    </>
+  );
+}
