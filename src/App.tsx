1e9782097a402f52cfbf15b882dde64a07899582
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-12 22:32:56 +0900

    study hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { ReactHookFormExample } from "./components/react-hook-form-example";
-import { ReactHookFormExample2 } from "./components/react-hook-form-example2";
+// import { ReactHookFormExample } from "./components/react-hook-form-example";
+// import { ReactHookFormExample2 } from "./components/react-hook-form-example2";
+import { ReactHookFormExample3 } from "./components/react-hook-form-example3";
+import { ReactHookFormExample4 } from "./components/react-hook-form-example4";
 
 export default function App() {
   return (
     <>
-      <ReactHookFormExample />;
-      <ReactHookFormExample2 />;
+      {/* <ReactHookFormExample />; */}
+      {/* <ReactHookFormExample2 />; */}
+      <ReactHookFormExample3 />;
+      <ReactHookFormExample4 />;
     </>
   );
 }
