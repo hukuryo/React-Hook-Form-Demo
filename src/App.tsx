5d3bb1b98fcc557d8b03f3735b8a6fc8f920e3dd
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-20 09:39:51 +0900

    グローバル状態との統合について学習

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 // import { ReactHookFormExample } from "./components/react-hook-form-example";
 // import { ReactHookFormExample2 } from "./components/react-hook-form-example2";
-import { ReactHookFormExample3 } from "./components/react-hook-form-example3";
-import { ReactHookFormExample4 } from "./components/react-hook-form-example4";
+// import { ReactHookFormExample3 } from "./components/react-hook-form-example3";
+// import { ReactHookFormExample4 } from "./components/react-hook-form-example4";
+import { ReactHookFormExample5 } from "./components/react-hook-form-example5";
 
 export default function App() {
   return (
     <>
       {/* <ReactHookFormExample />; */}
       {/* <ReactHookFormExample2 />; */}
-      <ReactHookFormExample3 />;
-      <ReactHookFormExample4 />;
+      {/* <ReactHookFormExample3 />;
+      <ReactHookFormExample4 />; */}
+      <ReactHookFormExample5 />;
     </>
   );
 }
---
+++ src/components/react-hook-form-example5.tsx
@@ -0,0 +1,48 @@
+import Select from "react-select";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import Input from "@mui/material/Input";
+
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  iceCreamType: { label: string; value: string };
+}
+
+export const ReactHookFormExample5 = () => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      iceCreamType: { label: "", value: "" },
+    },
+  });
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="firstName"
+        control={control}
+        render={({ field }) => <Input {...field} />}
+      />
+      <Controller
+        name="iceCreamType"
+        control={control}
+        render={({ field }) => (
+          <Select
+            {...field}
+            options={[
+              { value: "chocolate", label: "Chocolate" },
+              { value: "strawberry", label: "Strawberry" },
+              { value: "vanilla", label: "Vanilla" },
+            ]}
+          />
+        )}
+      />
+      <input type="submit" />
+    </form>
+  );
+};
